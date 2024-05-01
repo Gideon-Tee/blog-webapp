3a3423c69ca0e9db8a83a1e8aa225eecc6a9d3a1
Gideon Tettehfio <gideonlambride@gmail.com>
2024-05-01 15:04:14 +0000

    Interface completed

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Blog
 # Create your views here.
+
+def index(request):
+    blogs = Blog.objects.all()
+    context = {
+    'blogs': blogs
+    }
+
+    return render(request, 'index.html', context)
