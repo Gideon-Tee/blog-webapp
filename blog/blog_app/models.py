627947148d68518be460ae86f095e89901f88cbb
Gideon Tettehfio <gideonlambride@gmail.com>
2024-05-01 17:48:34 +0000

    basic backend Functionality added

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
     author = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -9,3 +9,11 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def post_detail(request, post_id):
+    blog = Blog.objects.get(id=post_id)
+    context = {
+        'post_id': post_id,
+        'blog': blog
+    }
+    return render(request, 'post_detail.html', context)
